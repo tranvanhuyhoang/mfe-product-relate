f5e40c1089596fc3291390868e100b1f764a3dea
hoangtvh <tranvanhuyhoang@gmail.com>
2022-07-06 11:16:44 +0700

    edit vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,18 @@
-// const { defineConfig } = require("@vue/cli-service")
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-//   lintOnSave: false,
-//   configureWebpack: {
-//     devServer: {
-//       headers: {
-//         "Access-Control-Allow-Origin": "*",
-//       },
-//     },
-//     externals: ["vue", "vue-router"],
-//     output: {
-//       libraryTarget: "system",
-//       // filename: "js/app.js",
-//     },
-//   },
-//   filenameHashing: false,
-// })
 const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: true,
+  lintOnSave: false,
   configureWebpack: {
+    devServer: {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    },
+    externals: ["vue", "vue-router", "mfe-share-lib"],
     output: {
       libraryTarget: "system",
       filename: "js/app.js",
     },
   },
+  filenameHashing: false,
 })
